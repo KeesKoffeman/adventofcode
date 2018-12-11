a80ef6fb5f95bab7de23c2a0c21eb05f2421c2ec
Kees Koffeman <kees@koffeman.net>
2018-12-11 23:25:10 +0100

    Day 11 of Advent of Code 2018 - refactored with summed area table alghoritm.

--- src/main/kotlin/net/koffeman/aoc/2018/Day11.kt
+++ src/main/kotlin/net/koffeman/aoc/2018/Day11.kt
@@ -4,42 +4,36 @@ import net.koffeman.aoc.AdventOfCodePuzzle
 
 class Day11 : AdventOfCodePuzzle {
 
-    override fun part1(input: List<String>) = Grid(input.first().toInt()).select(2).let { "${it.first.x},${it.first.y}" }
-
-    override fun part2(input: List<String>) = Grid(input.first().toInt()).select(20).let { "${it.first.x},${it.first.y},${it.second}" }
+    override fun part1(input: List<String>) = Grid(input.first().toInt()).largestFuelSquare(2).let { "${it.start.x},${it.start.y}" }
+    override fun part2(input: List<String>) = Grid(input.first().toInt()).largestFuelSquare(300).let { "${it.start.x},${it.start.y},${it.size}" }
 
     class Grid(private val serialNumber:Int) {
 
         val map : MutableMap<Int,MutableMap<Int, FuelCell>> = mutableMapOf()
 
-        init {
-                (1 .. 300).forEach { x -> (1 .. 300).forEach { y ->
+        init { (1 .. 300).forEach { x -> (1 .. 300).forEach { y ->
                     val rackId = x + 10
-                    val powerLevel = with(((rackId * y) + serialNumber) * rackId) { val str = toString();str[str.length - 3].toString().toInt() - 5 }
+                    val powerLevel = (((((rackId * y) + serialNumber) * rackId) / 100) % 10) - 5
                     map.computeIfAbsent(x) { mutableMapOf() }[y] = FuelCell(x,y,rackId,powerLevel)
                 } }
         }
 
-        fun select(maxSize : Int) : Pair<FuelCell,Int> {
-
-            val powerLevelPerSquare : MutableMap<Int,Pair<FuelCell, Int>> = mutableMapOf()
-            (1 .. 300).forEach { gridY -> (1 .. 300).forEach { gridX ->
-
-                val ceiledMaxSize = Math.min(maxSize, (300-Math.max(gridX, gridY)) )
-
-                (1 .. ceiledMaxSize).forEach { size ->
-                    val powerLevel = (gridY..gridY + size).map { sY -> (gridX..gridX + size).map { sX -> sX to sY } }.flatten().sumBy { map[it.first]!![it.second]!!.powerLevel }
-                    if (!powerLevelPerSquare.containsKey(powerLevel)) {
-                        powerLevelPerSquare[powerLevel] = map[gridX]!![gridY]!! to size+1
+        fun largestFuelSquare(maxSize : Int) : FuelSquare {
+            return map.values
+                    .flatMap { it.values }
+                    .flatMap { var powerlLevelSum = 0
+                        (0 .. Math.min(maxSize, (300-Math.max(it.x, it.y)))).map { size ->
+                                (it.x .. it.x + size).forEach { dx -> powerlLevelSum += map[dx]!![it.y + size]!!.powerLevel }
+                                (it.y until it.y + size).forEach { dy -> powerlLevelSum += map[it.x + size]!![dy]!!.powerLevel }
+                            FuelSquare(it, size+1, powerlLevelSum)
+                            }
                     }
-                }
-            }}
-
-            return powerLevelPerSquare[powerLevelPerSquare.keys.max()]!!
+                    .maxBy { it.powerLevel }!!
         }
     }
 
     data class FuelCell(val x: Int, val y: Int, val rackId: Int, val powerLevel: Int)
+    data class FuelSquare(val start : FuelCell, val size:Int, val powerLevel: Int)
 }
 
 fun main(args: Array<String>) = Day11().solve()
